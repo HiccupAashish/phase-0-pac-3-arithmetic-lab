7b7070524db25809051d8d0f8085096ca80c46ba
HiccupAashih <shahaashish22@gmail.com>
2022-02-25 15:18:09 +1100

    Test Completed

--- test/index-test.js
+++ test/index-test.js
@@ -1,29 +1,5 @@
-var a, b
-function  add(a,b){
-  return a+b;
-}
-function  subtract(a,b){
-  return a-b;
-}
-function  multiply(a,b){
-  return a*b;
-}
-function  divide(a,b){
-  return a/b;
-}
-function increment(n){
-  return n+=1;
-}
-function decrement(n){
-  return n-=1;
-}
 
-function makeInt(n){
-  return (parseInt('n', 10));
-}
-function preserveDecimal(n){
-  return parseFloat('n');
-}
+
 
 beforeEach(function() {
   a = Math.floor(Math.random() * 1000)
